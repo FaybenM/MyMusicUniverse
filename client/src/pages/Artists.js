009d311ad0c5e580aea75d7b701c4fed35cd8c24
Fayben <faybenm98@gmail.com>
2025-02-24 13:33:49 -0500

    update artist.js

--- client/src/pages/Artists.js
+++ client/src/pages/Artists.js
@@ -1,23 +1,32 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 function Artists() {
   const [artists, setArtists] = useState([]);
 
   useEffect(() => {
-    axios.get("http://localhost:5050/api/artists")
-      .then(response => setArtists(response.data))
-      .catch(error => console.error("Error fetching artists:", error));
+    axios.get("http://localhost:5050/api/artists") // Adjust if your API is different
+      .then(response => {
+        console.log(response.data); // Check what is being received
+        setArtists(response.data);
+      })
+      .catch(error => {
+        console.error("Error fetching artists:", error);
+      });
   }, []);
 
   return (
     <div>
       <h1>Artists</h1>
-      <ul>
-        {artists.map((artist) => (
-          <li key={artist._id}>{artist.name}</li>
-        ))}
-      </ul>
+      {artists.length === 0 ? (
+        <p>No artists found.</p>
+      ) : (
+        <ul>
+          {artists.map((artist) => (
+            <li key={artist.id}>{artist.name}</li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
