e19d0e02dedfc60bf4189ef7e3cadd435d2db8e3
Fayben <faybenm98@gmail.com>
2025-02-26 16:22:36 -0500

    add logo

--- client/src/App.js
+++ client/src/App.js
@@ -7,20 +7,26 @@ import ArtistDetail from "./pages/ArtistDetail";
 import Albums from "./pages/Albums";
 import Concerts from "./pages/Concerts";
 import Admin from "./pages/Admin";
+import logo from "./images/MyMusicUniverseLogo.png";
 
 
 function App() {
   return (
     <Router>
-      <nav>
-        <ul>
+      <header>
+        <nav className="navbar">
+          <div className="logo">
+            <img src={logo} alt="My Music Universe Logo" />
+          </div>
+          <ul className="nav-links">
           <li><Link to="/">Home</Link></li>
           <li><Link to="/artists">Artists</Link></li>
           <li><Link to="/albums">Albums</Link></li>
           <li><Link to="/concerts">Concerts</Link></li>
           <li><Link to="/admin">Admin</Link></li>
-        </ul>
-      </nav>
+          </ul>
+        </nav>
+     </header>
 
       <Routes>
         <Route path="/" element={<Home />} />
